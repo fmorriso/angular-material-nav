9d3b3989513d771d573fcbbacfa7280e5864e88a
Fred Morrison <fpmorrison@verizon.net>
2018-08-15 17:18:11 -0400

    Upgrade to Angular 6 and Angular-CLI 6

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, VERSION} from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  angularVersion: string = VERSION.full;
   constructor() { }
 
   ngOnInit() {
