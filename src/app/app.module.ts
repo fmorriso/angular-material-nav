ec9f1d3bdfc737e88e4a95c053365b4c0c959f1a
Fred Morrison <fpmorrison@verizon.net>
2019-03-19 21:17:02 -0400

    flex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { NavigationComponent } from './navigation/navigation.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
--- src/app/picture-grid/picture-grid.component.ts
+++ src/app/picture-grid/picture-grid.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
+
+
 import { Tile } from './tile';
 
 @Component({
