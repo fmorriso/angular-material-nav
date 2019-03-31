dbe7a1477029ff90cbbd14c9f8f15bb71f9e3b99
Fred Morrison <fpmorrison@verizon.net>
2019-03-31 12:45:08 -0400

    reorder imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-
-import { AppComponent } from './app.component';
+//
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './shared/material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
+//
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
