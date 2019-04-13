b629c8260c3e6cfb44a76a98ef1bf947afacf6f4
Fred Morrison <fpmorrison@verizon.net>
2019-04-13 13:17:44 -0400

    remove directory

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule, ExtraOptions } from '@angular/router';
+//
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
