67fb50a9b02148dfffec61b491c766647d6705d0
Fred Morrison <fpmorrison@verizon.net>
2020-02-11 08:34:07 -0500

    Update custom.material.module.ts

--- src/app/shared/custom.material.module.ts
+++ src/app/shared/custom.material.module.ts
@@ -11,10 +11,12 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
+import { MatIconRegistry} from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MAT_LABEL_GLOBAL_OPTIONS} from '@angular/material/core';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
-
+import { ModuleWithProviders} from '@angular/core';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
@@ -89,6 +91,7 @@ import { ObserversModule } from '@angular/cdk/observers';
     MatFormFieldModule,
     MatGridListModule,
     MatIconModule,
+    MatIconRegistry,
     MatInputModule,
     MatListModule,
     MatMenuModule,
